c38318ea7ba732b03f9f3cd885655f13c3c78205
Rajath Gokhale <rajathgokhale@Mac.lan>
2025-04-13 19:50:28 -0400

    add congnito functionality

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,16 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "cognito66c96222": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    },
+    "userPoolGroups": {
+      "MyPoolGroupRole": "string"
+    }
+  }
+}
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -13,6 +14,14 @@ import ClientListPage from "@/pages/client-list";
 import DataHistoryPage from "@/pages/data-history";
 import LoadingPage from "@/pages/loading";
 import NavigationPage from "@/pages/navigation";
+
+// Import Amplify, aws-exports, and the withAuthenticator HOC from Amplify UI
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+
+// Configure Amplify with your AWS resources (Cognito, etc.)
+Amplify.configure(awsconfig);
 
 function Router() {
   return (
@@ -46,4 +55,6 @@ function App() {
   );
 }
 
-export default App;
+// Wrap your App with withAuthenticator to enable Cognito authentication
+export default withAuthenticator(App);
+
